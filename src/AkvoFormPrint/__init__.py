7c53c79b55bacd00896507648bfd3910cb1a92b9
wayangalihpratama <wgprtm@gmail.com>
2025-06-13 05:51:04 +0000

    Release 0.1.0b3

--- src/AkvoFormPrint/__init__.py
+++ src/AkvoFormPrint/__init__.py
@@ -2,6 +2,6 @@
 AkvoFormPrint - Convert form definitions to PDF/HTML with custom styling
 """
 
-__version__ = "0.1.0b1"
+__version__ = "0.1.0b3"
 __author__ = "Akvo"
 __email__ = "tech.consultancy@akvo.org"
